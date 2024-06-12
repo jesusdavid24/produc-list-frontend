9c27c3ed9569eb2551ce7f1c89047157341928e3
Jesus Bravo <jd_bravo24@hotmail.com>
2024-06-12 02:14:33 -0500

    identation

--- src/App.jsx
+++ src/App.jsx
@@ -6,134 +6,134 @@ import TableForm from "./components/Table";
 import Contact from "./components/Contact";
 import "./App.scss";
 
-import { 
-   getProduct, 
-   postProduct, 
-   editProduct, 
-   deleteProduct 
+import {
+  getProduct,
+  postProduct,
+  editProduct,
+  deleteProduct
 } from "./api/product";
 
 
 function App() {
-   const [viewPage, setViewPage] = useState("products");
-   const [showForm, setShowForm] = useState(false);
-   const [isEdit, setIsEdit] = useState(false);
-   const [isLoading, setIsLoading] = useState(true);
-   const [products, setProducts] = useState([]);
-   const [productToEdit, setProductToEdit] = useState(null);
+  const [viewPage, setViewPage] = useState("products");
+  const [showForm, setShowForm] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+  const [productToEdit, setProductToEdit] = useState(null);
 
-   useEffect(() => {
-      getProduct().then((x) => {
-         setProducts(x);
-         setIsLoading(false);
-      });
-   }, []);
+  useEffect(() => {
+    getProduct().then((x) => {
+      setProducts(x);
+      setIsLoading(false);
+    });
+  }, []);
 
-   const handleAddButtonClick = () => {
-      !showForm && (setIsEdit(false), setProductToEdit(null));
-      setShowForm(!showForm);
-   };
+  const handleAddButtonClick = () => {
+    !showForm && (setIsEdit(false), setProductToEdit(null));
+    setShowForm(!showForm);
+  };
 
-   const handleViewPage = (newViewPage) => {
-      setViewPage(newViewPage);
-   };
+  const handleViewPage = (newViewPage) => {
+    setViewPage(newViewPage);
+  };
 
-   const handleAddProduct = async (newProduct) => {
-      const { id } = newProduct;
-      if (isEdit) {
-         const data = await editProduct(id, newProduct);
+  const handleAddProduct = async (newProduct) => {
+    const { id } = newProduct;
+    if (isEdit) {
+      const data = await editProduct(id, newProduct);
 
-         const editedProductsList = products.map((product) =>
-            product.id == data.id ? data : product
-         );
+      const editedProductsList = products.map((product) =>
+        product.id == data.id ? data : product
+      );
 
-         setProducts(editedProductsList);
-         setShowForm(false)
-         
-      } else {
-         const data = await postProduct(newProduct);
-         setProducts([...products, data]);
-         setShowForm(false)
-      }
-   };
+      setProducts(editedProductsList);
+      setShowForm(false)
 
-   const handleDeleteProduct = async (id) => {
-      await deleteProduct(id), setProducts(products.filter((product) => product.id !== id));
-   };
+    } else {
+      const data = await postProduct(newProduct);
+      setProducts([...products, data]);
+      setShowForm(false)
+    }
+  };
 
-   const handleEditProduct = (productToEdit) => {
-      setProductToEdit(productToEdit);
-      setIsEdit(true);
-      setShowForm(true);
-   };
+  const handleDeleteProduct = async (id) => {
+    await deleteProduct(id), setProducts(products.filter((product) => product.id !== id));
+  };
 
-   return (
-      <div className="container-fluid p-0">
-         <Header handleViewPage={handleViewPage} />
+  const handleEditProduct = (productToEdit) => {
+    setProductToEdit(productToEdit);
+    setIsEdit(true);
+    setShowForm(true);
+  };
 
-         {viewPage === "products" ? (
-            <div className="app-form">
-               <div className="table-container">
-                  <div className="presentation">
-                     <h1 className="presentation__title">Products List</h1>
-                     <button className="presentation__add-button" onClick={handleAddButtonClick}>
-                        {showForm ? "Hide" : "Add"}
-                     </button>
-                  </div>
+  return (
+    <div className="container-fluid p-0">
+      <Header handleViewPage={handleViewPage} />
 
-                  {isLoading ? (
-                     <div className="dot-spinner">
-                        <div className="dot-spinner__dot"></div>
-                        <div className="dot-spinner__dot"></div>
-                        <div className="dot-spinner__dot"></div>
-                        <div className="dot-spinner__dot"></div>
-                        <div className="dot-spinner__dot"></div>
-                        <div className="dot-spinner__dot"></div>
-                        <div className="dot-spinner__dot"></div>
-                        <div className="dot-spinner__dot"></div>
-                     </div> 
-                  ) : (
-                     <TableForm
-                        products={products}
-                        onDeleteProduct={handleDeleteProduct}
-                        onEditProduct={handleEditProduct}
-                     />
-                  )}
-
-               </div>
-
-               {showForm && (
-                  <ProductForm
-                     title={isEdit ? "Edit Product" : "Add Product"}
-                     products={products}
-                     isEdit={isEdit}
-                     onAddProduct={handleAddProduct}
-                     onEditProduct={handleEditProduct}
-                     productToEdit={productToEdit}>
-                     {isEdit ? (
-                        <div className="edit-buttons-container">
-                           <button
-                              type="button"
-                              className="form__cancel-button"
-                              onClick={() => setShowForm(false)}>
-                              Cancel
-                           </button>
-                           <button type="submit" className="form__update-button">
-                              Update
-                           </button>
-                        </div>
-                     ) : (
-                        <button type="submit" className="form__add-button">
-                           Add
-                        </button>
-                     )}
-                  </ProductForm>
-               )}
+      {viewPage === "products" ? (
+        <div className="app-form">
+          <div className="table-container">
+            <div className="presentation">
+              <h1 className="presentation__title">Products List</h1>
+              <button className="presentation__add-button" onClick={handleAddButtonClick}>
+                {showForm ? "Hide" : "Add"}
+              </button>
             </div>
-         ) : (
-            <Contact />
-         )}
-      </div>
-   );
+
+            {isLoading ? (
+              <div className="dot-spinner">
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+                <div className="dot-spinner__dot"></div>
+              </div>
+            ) : (
+              <TableForm
+                products={products}
+                onDeleteProduct={handleDeleteProduct}
+                onEditProduct={handleEditProduct}
+              />
+            )}
+
+          </div>
+
+          {showForm && (
+            <ProductForm
+              title={isEdit ? "Edit Product" : "Add Product"}
+              products={products}
+              isEdit={isEdit}
+              onAddProduct={handleAddProduct}
+              onEditProduct={handleEditProduct}
+              productToEdit={productToEdit}>
+              {isEdit ? (
+                <div className="edit-buttons-container">
+                  <button
+                    type="button"
+                    className="form__cancel-button"
+                    onClick={() => setShowForm(false)}>
+                    Cancel
+                  </button>
+                  <button type="submit" className="form__update-button">
+                    Update
+                  </button>
+                </div>
+              ) : (
+                <button type="submit" className="form__add-button">
+                  Add
+                </button>
+              )}
+            </ProductForm>
+          )}
+        </div>
+      ) : (
+        <Contact />
+      )}
+    </div>
+  );
 }
 export default App;
--- src/api/product.js
+++ src/api/product.js
@@ -3,45 +3,45 @@ import axios from 'axios';
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const getProduct = async () => {
-   try {
-      const response = await axios.get(`${apiBaseUrl}/product`);
-      return response.data;
-   } catch (error) {
-      throw new Error("An error has occurred");
-   }
+  try {
+    const response = await axios.get(`${apiBaseUrl}/product`);
+    return response.data;
+  } catch (error) {
+    throw new Error("An error has occurred");
+  }
 };
 
 export const postProduct = async (body) => {
-   try {
-      const response = await axios.post(`${apiBaseUrl}/product`, body, {
-         headers: {
-            "Content-Type": "application/json",
-         },
-      });
-      return response.data;
-   } catch (error) {
-      throw new Error("An error has occurred");
-   }
+  try {
+    const response = await axios.post(`${apiBaseUrl}/product`, body, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("An error has occurred");
+  }
 };
 
 export const editProduct = async (id, body) => {
-   try {
-      const response = await axios.put(`${apiBaseUrl}/product/${id}`, body, {
-         headers: {
-            "Content-Type": "application/json",
-         },
-      });
-      return response.data;
-   } catch (error) {
-      throw new Error("An error has occurred");
-   }
+  try {
+    const response = await axios.put(`${apiBaseUrl}/product/${id}`, body, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("An error has occurred");
+  }
 };
 
 export const deleteProduct = async (id) => {
-   try {
-      const response = await axios.delete(`${apiBaseUrl}/product/${id}`);
-      return response.data;
-   } catch (error) {
-      throw new Error("An error has occurred");
-   }
+  try {
+    const response = await axios.delete(`${apiBaseUrl}/product/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error("An error has occurred");
+  }
 };
